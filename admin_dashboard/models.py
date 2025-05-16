5a346dbba70031c1ccadd4bd127031e92f84046d
d1knight <mdo255da@gmail.com>
2025-05-16 16:52:50 +0500

    back

--- admin_dashboard/models.py
+++ admin_dashboard/models.py
@@ -26,61 +26,8 @@ class Service(models.Model):
     def __str__(self):
         return self.name
 
-<<<<<<< HEAD
 class Speciality(models.Model):
     name = models.CharField(max_length=100)
-=======
-
-
-
-class Visit(models.Model):
-    patient = models.ForeignKey('Patient', on_delete=models.CASCADE)
-    services = models.ManyToManyField('Service')
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    paid_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    remaining_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    payment_type = models.JSONField()
-
-    # Добавляем поле payment_status
-    PAYMENT_STATUS_CHOICES = [
-        ('pending', 'Ожидает оплаты'),
-        ('paid', 'Оплачен'),
-    ]
-    payment_status = models.CharField(
-        max_length=10,
-        choices=PAYMENT_STATUS_CHOICES,
-        default='pending',
-    )
-
-    def calculate_total_price(self):
-        """
-        Рассчитываем итоговую стоимость на основе связанных услуг.
-        """
-        return sum(service.price for service in self.services.all())
-
-    def save(self, *args, **kwargs):
-        """
-        Переопределённый метод save:
-        - Сохраняем объект без расчёта total_price для избежания проблем с ManyToMany.
-        - После сохранения объекта и установки связей, обновляем total_price.
-        - Обновляем статус оплаты.
-        """
-        is_new = self.pk is None  # Проверяем, новый ли это объект
-        super().save(*args, **kwargs)  # Сохраняем объект
-
-        # После сохранения (и только для существующих объектов) пересчитываем total_price
-        if not is_new:
-            self.total_price = self.calculate_total_price()
-            self.remaining_amount = self.total_price - self.paid_amount
-
-            # Обновляем статус оплаты
-            if self.remaining_amount <= 0:
-                self.payment_status = 'paid'
-            else:
-                self.payment_status = 'pending'
-            
-            super().save(update_fields=['total_price', 'remaining_amount', 'payment_status'])  # Сохраняем только изменённые поля
->>>>>>> c57154d27fe01d8dac89a2b16848664be7283ab9
 
     def __str__(self):
         return self.name
--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from django.contrib import messages
-<<<<<<< HEAD
 from django.template import loader
 from .models import Patient, Service, Visit
 from decimal import Decimal, ROUND_HALF_UP
@@ -12,9 +11,6 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 import os
 from django.conf import settings
-=======
-from django.views.decorators.csrf import csrf_exempt
->>>>>>> c57154d27fe01d8dac89a2b16848664be7283ab9
 
 
 def show_dashboard(request):
@@ -26,57 +22,7 @@ def patient_list(request):
     paginator = Paginator(patients, 10)  # Пагинация по 10 записи
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-<<<<<<< HEAD
     return render(request, 'admin_dashboard/patient_list.html', {'page_obj': page_obj})
-=======
-
-    context = {
-        'page_obj': page_obj,
-        'search_query': search_query,
-        'sort_by': sort_by,
-    }
-    return render(request, 'admin_dashboard/visit_list.html', context)
-
-
-def visit_print(request, visit_id):
-    visit = get_object_or_404(Visit, id=visit_id)
-    template = loader.get_template('admin_dashboard/visit_print.html')
-    context = {'visit': visit}
-    html = template.render(context)
-
-    # Временная заглушка: выводим HTML как PDF
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename="visit_{visit.id}.pdf"'
-    response.write(html.encode('utf-8'))  # PDF-генерация будет добавлена позже
-    return response
-
-@csrf_exempt
-def visit_payment(request, visit_id):
-    visit = Visit.objects.get(id=visit_id)
-
-    # Проверяем, если визит уже оплачен, перенаправляем обратно с сообщением
-    if visit.payment_status == 'paid':
-        return redirect('admin_dashboard:visit_list')  # Или перенаправьте на другую страницу
-
-    if request.method == 'POST':
-        payment_method = request.POST.get('payment_method')
-        paid_amount = float(request.POST.get('paid_amount', 0))
-
-        # Добавляем оплаченную сумму к общей    
-        visit.paid_amount += paid_amount
-        visit.remaining_amount = visit.total_price - visit.paid_amount
-
-        # Проверяем, если оставшаяся сумма 0, обновляем статус на 'paid'
-        if visit.remaining_amount <= 0:
-            visit.payment_status = 'paid'
-
-        # Сохраняем изменения
-        visit.save()
-
-        # return redirect('admin_dashboard:visit_list')  # Перенаправляем на список визитов
-
-    return render(request, 'admin_dashboard/visit_payment.html', {'visit': visit})
->>>>>>> c57154d27fe01d8dac89a2b16848664be7283ab9
 
 
 def patient_add(request):
